ef08e2be4ba7032514e0e0ad215df581706a7fa5
Isaac Emmanuel <isaacnsisong@gmail.com>
2024-12-26 19:47:04 +0100

    Auto-commit on 2024-12-26 19:47:03

---
+++ include/config_parser.h
@@ -0,0 +1,6 @@
+#ifndef CONFIG_PARSER_H
+#define CONFIG_PARSER_H
+
+int load_config(const char *filename);
+
+#endif
---
+++ src/config_parser.c
@@ -0,0 +1,19 @@
+#include "include/config_parser.h"
+#include <stdio.h>
+#include <string.h>
+
+int load_config(const char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        perror("fopen");
+        return 0;
+    }
+
+    char line[256];
+    while (fgets(line, sizeof(line), file)) {
+        printf("Config: %s", line);
+    }
+
+    fclose(file);
+    return 1;
+}
